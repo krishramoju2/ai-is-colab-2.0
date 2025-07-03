8a18864fef1768183eb09499ed318cd35db763c6
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-04 04:38:14 +0530

    Update TypingLine.tsx

--- components/TypingLine.tsx
+++ components/TypingLine.tsx
@@ -1,1 +1,12 @@
+'use client';
+import React from 'react';
+
+export default function TypingLine() {
+  return (
+    <div className="text-gray-400 italic animate-pulse">
+      🤖 Bots are thinking...
+    </div>
+  );
+}
+
 
