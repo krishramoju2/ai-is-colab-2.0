c9b219febbcbe65f6720bb4b2591058b7a415989
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-05 19:38:48 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,19 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
         calligraphy1: ['"Great Vibes"', 'cursive'],
         calligraphy2: ['"Dancing Script"', 'cursive'],
         calligraphy3: ['"Satisfy"', 'cursive'],
         calligraphy4: ['"Allura"', 'cursive'],
         calligraphy5: ['"Pacifico"', 'cursive'],
+        lobster: ['Lobster', 'cursive'],
+        sacramento: ['Sacramento', 'cursive'],
+        shadows: ['"Shadows Into Light"', 'cursive'],
+        calligraffitti: ['Calligraffitti', 'cursive'],
       },
     },
   },
